288e47777559d59fdd5152c06fe5f2b4dddc6287
liangshiyuan <731974876@qq.com>
2020-09-14 13:04:51 +0800

    初始化

---
+++ src/main/java/com/example/psm/controller/PsmController.java
@@ -0,0 +1,24 @@
+package com.example.psm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lsy
+ * @version 1.0
+ * @date 2020-09-11 12:42
+ **/
+@RestController
+@RequestMapping("psm/app")
+public class PsmController {
+
+
+    @RequestMapping(value = "demo")
+    public String demo(){
+
+        return "第一个springboot项目";
+    }
+
+
+
+}
